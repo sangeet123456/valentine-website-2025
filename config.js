64ef82faf952b124063bffc2a413653ff6c8ad12
sangeet123456 <sangeetparajuli9@gmail.com>
2026-01-31 22:10:28 +0545

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dfvujrak2/video/upload/v1769876238/Yabesh_Thapa_-_Kasari_%E0%A4%95%E0%A4%B8%E0%A4%B0_vnzjut.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dfvujrak2/video/upload/v1769876689/Kalank_Title_Track_-_Lyrical_Alia_Bhatt_Varun_Dhawan_Arijit_Singh_Pritam_Amitabh_tv92gu.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
